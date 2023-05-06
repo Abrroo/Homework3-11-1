cf457a6bded6be2758106a5b5df740670d5f06a1
Ersyn <akhmetov.ersyn@gmail.com>
2023-05-06 18:22:17 +0300

    авыа

---
+++ homework3-11-1/homework3-11-1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include "GreeterStaticLib.h"
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    std::string str;
+    std::cout << "Введите имя: ";
+    std::cin >> str;
+    Greeter C;
+    C.greet(str);
+}
